8d5c6152f858b8d771d2de30024b4c52b0f4bbc5
Hasib787 <mr.hasib45@gmail.com>
2021-03-30 04:25:27 +0600

    load data from database for a specific user

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ client.connect(err => {
         })
         console.log(newBooking);
     })
+
+    app.get('/bookings', (req, res)=>{
+        // console.log(req.query.email);
+        bookings.find({email: req.query.email})
+        .toArray((err, documents)=>{
+            res.send(documents)
+        })
+    })
   });
 
 app.get('/', (req, res) => {
