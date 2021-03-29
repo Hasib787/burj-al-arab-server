cfe29f81a3aa7bcaac61449fade26e1ebf5615d6
Hasib787 <mr.hasib45@gmail.com>
2021-03-30 03:39:36 +0600

    making burj al arab server site

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const password = "Hasib42742";
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://burjalarab:Hasib42742@cluster0.fg2cz.mongodb.net/burjAlArab?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+client.connect(err => {
+    const bookings = client.db("burjAlArab").collection("bookings");
+        
+    app.post('/addBooking', (req, res)=>{
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+        .then(result =>{
+            res.send(result.insertedCount > 0);
+        })
+        console.log(newBooking);
+    })
+  });
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(5000)
