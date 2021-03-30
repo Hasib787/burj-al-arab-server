00355e10552aef2042bd43dc4cf3d4ee33f16dca
Hasib787 <mr.hasib45@gmail.com>
2021-03-31 03:32:50 +0600

    verify jwt token in the server end using firebase admin

--- index.js
+++ index.js
@@ -12,29 +12,57 @@ const app = express()
 app.use(cors());
 app.use(bodyParser.json());
 
+const admin = require("firebase-admin");
+const serviceAccount = require("./burj-al-arab-ahotel-firebase-adminsdk-mcfpb-204fb8913d.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 client.connect(err => {
     const bookings = client.db("burjAlArab").collection("bookings");
-        
-    app.post('/addBooking', (req, res)=>{
+
+    app.post('/addBooking', (req, res) => {
         const newBooking = req.body;
         bookings.insertOne(newBooking)
-        .then(result =>{
-            res.send(result.insertedCount > 0);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
         console.log(newBooking);
     })
 
-    app.get('/bookings', (req, res)=>{
-        console.log(req.headers.authorization);
-        bookings.find({email: req.query.email})
-        .toArray((err, documents)=>{
-            res.send(documents)
-        })
+    app.get('/bookings', (req, res) => {
+        const bearer = req.headers.authorization;
+        if (bearer && bearer.startsWith('Bearer ')) {
+            const idToken = bearer.split(' ')[1];
+            console.log({ idToken });
+            admin
+                .auth()
+                .verifyIdToken(idToken)
+                .then((decodedToken) => {
+                    const tokenEmail = decodedToken.email;
+                    const queryEmail = req.query.email;
+                    console.log(tokenEmail, queryEmail);
+                    if (tokenEmail === queryEmail) {
+                        bookings.find({ email: req.query.email })
+                            .toArray((err, documents) => {
+                                res.send(documents)
+                            })
+                    }
+                    // ...
+                })
+                .catch((error) => {
+                    // Handle error
+                });
+
+        }
+
+
+
     })
-  });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  
-  app.listen(5000)
+})
+
+app.listen(5000)
